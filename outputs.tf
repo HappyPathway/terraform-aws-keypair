4971a7b3e98bf23b502acf7f5551ef8c4fbf07b3
Dave Arnold <darnold@hashicorp.com>
2018-06-21 15:36:29 -0700

    removing variables from outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,3 @@
-# Optional Variables
-variable "private_key_filename" {
-  default     = "private_key.pem"
-  description = "Filename to write the private key data to eg key.pem"
-}
-
-# Required variables
-variable "ssh_key_name" {
-  description = "AWS key pair name that will be created"
-}
-
 # Outputs
 output "private_key_data" {
   value = "${tls_private_key.main.private_key_pem}"
